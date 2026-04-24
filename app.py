f3bca354c77bcd99af6f28e0e8464a89e7d78b47
rafflores17-prog <rafflores17@gmail.com>
2026-04-24 08:08:52 -0300

    Add jsonify import to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, jsonify
 import requests
 import random
 import re
